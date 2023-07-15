5c1db7a6830f6ef39ec662d1d56f4b6c1a406737
Nishchay Ch <chaurasiyanishchay@gmail.com>
2023-07-15 19:14:27 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fs = require('fs');
+
+const PORT = process.env.PORT;
+
+const home = fs.readFileSync("./index.html");
+
+const server = http.createServer((req, res)=>{
+
+
+    if(req.url==='/')
+    {
+        return res.end(home);
+    }
+    if(req.url==="/about")
+    {
+        return res.end("<h1>About</h1>");
+    }
+    if(req.url==="/contact")
+    {
+        return res.end("<h1>CONTACT PAGE</h1>");
+    }
+    else{
+        return res.end("<h1> 404 NOT FOUND</h1>")
+    }
+})
+
+
+
+
+server.listen(PORT || 3000, ()=>{
+    console.log("Server is running");
+})
